83f1216b0a9b23b78d65ee8651ea391b848a8cad
AZIZEM81 <eaziz428@gmail.com>
2024-10-01 17:45:44 +0000

    Resolved merge conflicts

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,6 +9,7 @@ Module generating Pascal's Triangle
 
 from typing import List
 
+
 def pascal_triangle(n: int) -> List[List[int]]:
     """
     Generate Pascal's Triangle up to n rows.
@@ -21,7 +22,7 @@ def pascal_triangle(n: int) -> List[List[int]]:
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
     for _ in range(1, n):
         prev_row = triangle[-1]
@@ -30,5 +31,5 @@ def pascal_triangle(n: int) -> List[List[int]]:
             new_row.append(prev_row[j-1] + prev_row[j])
         new_row.append(1)
         triangle.append(new_row)
-    
+
     return triangle
