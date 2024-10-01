3018fb5c5b252f4acc1743584bce4b993a09c433
AZIZEM81 <eaziz428@gmail.com>
2024-10-01 18:26:26 +0000

    Resolved merge conflicts

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
 """
-<<<<<<< HEAD
-Module for generating Pascal's Triangle
-=======
-Module generating Pascal's Triangle
->>>>>>> 8c3b71e274935e08c46fed782fafe04956e8a5e0
+Implementing the pascals triangle
 """
 
-from typing import List
 
-
-def pascal_triangle(n: int) -> List[List[int]]:
+def pascal_triangle(n):
     """
-    Generate Pascal's Triangle up to n rows.
-
-    Args:
-        n (int): Number of rows to generate
-
-    Returns:
-        List[List[int]]: Pascal's Triangle as a list of lists of integers
+    The pascals triangle, which is a maths concept is a code that
+    return a list of intergents
     """
+
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    for _ in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j-1] + prev_row[j])
-        new_row.append(1)
-        triangle.append(new_row)
+    triangle = []
+    for i in range(n):
+        triangle.append([])
+        triangle[i].append(1)
+        if (i > 0):
+            for j in range(1, i):
+                triangle[i].append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            triangle[i].append(1)
 
     return triangle
